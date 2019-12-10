c0983d9bd556d7287bd41e4b05ee5d580606627d
bts7623 <37794016+bts7623@users.noreply.github.com>
2019-12-10 17:56:08 +0900

    #3.6 todo.js Commit
    #3.6 ing

--- todo.js
+++ todo.js
@@ -6,16 +6,32 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos"; //To Dos Local Storage
 
+const toDos = []; //To do list를 담을 Array
+
+function saveToDos(){ //입력된 Todos Array를 local Storage에 저장
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); //Array JS Object를 String으로 변환
+}
+
 function paintToDo(text) {//4. 화면에 뿌혀줌
     if(text !== ""){
         const li = document.createElement("li");//li 생성
         const delBtn = document.createElement("button");//삭제 버튼 생성
-        delBtn.innerHTML="❌";//버튼 이모지 추가
         const span = document.createElement("span");//span 추가
+        const newId = toDos.length + 1;
+        delBtn.innerHTML="❌";//버튼 이모지 추가
         span.innerText = text;//span에 text 추가
         li.appendChild(delBtn);//li에 delBtn 추가
         li.appendChild(span);//li에 span 추가
         toDoList.appendChild(li);//ul에 li 추가
+        li.id = newId;
+        
+        const toDoObj = { //To do list 가 추가될 때마다 Array에 넣어줌
+            text: text,
+            id: newId
+        };
+
+        toDos.push(toDoObj);
+        saveToDos();//push한 이후에 호출해야 locaStorage에 넣는다.
     }
 }
 
@@ -27,8 +43,12 @@ function handleSubmit(event) {//3. enter key 입력 시 화면에 뿌려줌
 }
 
 function loadToDos(){//2. Todo List를 가져오는 함수
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null){
+        console.log(loadedToDos);
+        //console.log(loadedToDos); String이기 때문에 JSON을 써서 Object로 변환 필요
+        const parsedToDos = JSON.parse(loadedToDos);
+        console.log(parsedToDos);
     }
 }
 
